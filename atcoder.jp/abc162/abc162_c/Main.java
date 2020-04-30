49340dab595a788372c9e78cf0809db02bb41231
Shigeki <android.s12.device@gmail.com>
2020-04-30 18:12:23 +0900

    [AC] abc162 abc162_c

---
+++ atcoder.jp/abc162/abc162_c/Main.java
@@ -0,0 +1,63 @@
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+
+    static String br = System.getProperty("line.separator");
+    static Scanner sc = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int N = nextInt();
+        long sum = 0;
+        for(int a=1; a<=N; a++){
+            for(int b=1; b<=N; b++){
+                for(int c=1; c<=N; c++){
+                    sum += gcd(gcd(a,b), c);
+                }
+            }
+        }
+        println(sum);
+    }
+
+    static long gcd (long a, long b) {
+        long temp;
+        while ((temp = a % b) != 0) {
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static Long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    static String next() {
+        return sc.next();
+    }
+
+    static String nextLine(){
+        return sc.nextLine();
+    }
+
+    static void println(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    static void print(Object o) {
+        PrintWriter out = new PrintWriter(System.out);
+        out.print(o);
+        out.flush();
+    }
+}
