a668e192a3cbf05f7e11a746ab0be206176bbce5
Shigeki <android.s12.device@gmail.com>
2020-04-30 18:02:57 +0900

    [AC] abc163 abc163_c

---
+++ atcoder.jp/abc163/abc163_c/Main.java
@@ -0,0 +1,52 @@
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+
+    static String br = System.getProperty("line.separator");
+    static Scanner sc = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int N = nextInt();
+        int[] num = new int[N];
+        for(int i=0; i<N-1; i++){
+            num[nextInt()-1] += 1;
+        }
+        for(int i=0; i<N; i++){
+            println(num[i]);
+        }
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static Long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    static String next() {
+        return sc.next();
+    }
+
+    static String nextLine(){
+        return sc.nextLine();
+    }
+
+    static void println(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    static void print(Object o) {
+        PrintWriter out = new PrintWriter(System.out);
+        out.print(o);
+        out.flush();
+    }
+}
