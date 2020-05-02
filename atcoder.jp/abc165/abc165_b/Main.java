0c7a313894ba758067c0971dc7f50fc174cd21b9
Shigeki <android.s12.device@gmail.com>
2020-05-02 21:52:14 +0900

    [AC] abc165 abc165_b

---
+++ atcoder.jp/abc165/abc165_b/Main.java
@@ -0,0 +1,55 @@
+import javax.print.DocFlavor;
+import java.awt.*;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+
+public class Main {
+
+    static String br = System.getProperty("line.separator");
+    static Scanner sc = new Scanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        long X = nextLong();
+
+        long a = 100;
+        for(long i=1; ;i++){
+            a = (long)(a*1.01);
+            if(a >= X){
+                println(i);
+                return;
+            }
+        }
+        //println((int)Math.ceil(Math.log(X/100.0)/Math.log(1.01)));
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    static Long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    static String next() {
+        return sc.next();
+    }
+
+    static String nextLine(){
+        return sc.nextLine();
+    }
+
+    static void println(Object o) {
+        out.println(o);
+        out.flush();
+    }
+
+    static void print(Object o) {
+        PrintWriter out = new PrintWriter(System.out);
+        out.print(o);
+        out.flush();
+    }
+}
