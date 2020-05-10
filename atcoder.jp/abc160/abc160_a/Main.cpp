61e18ad9ec8c7449288605132ea3037c2859493d
Shigeki <android.s12.device@gmail.com>
2020-05-10 14:40:23 +0900

    [AC] abc160 abc160_a

---
+++ atcoder.jp/abc160/abc160_a/Main.cpp
@@ -0,0 +1,9 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    cout << (S[2]==S[3] && S[4]==S[5] ? "Yes" : "No") << endl;
+}
