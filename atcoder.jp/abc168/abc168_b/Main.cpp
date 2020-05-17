9f213b51d6cdb2cab63a4c6f01941055e2814526
Shigeki <android.s12.device@gmail.com>
2020-05-17 21:04:02 +0900

    [AC] abc168 abc168_b

---
+++ atcoder.jp/abc168/abc168_b/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int K;
+    string S;
+     cin>>K>>S;
+
+     if(S.size() > K){
+         cout << S.substr(0, K) + "..." << endl;
+     }else{
+        cout << S << endl;
+     }
+}
+
