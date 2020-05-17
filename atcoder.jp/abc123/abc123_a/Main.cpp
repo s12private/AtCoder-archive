f05a3f37b86dc3a2f52d7fee6214700b5d576540
Shigeki <android.s12.device@gmail.com>
2020-05-17 19:45:58 +0900

    [AC] abc123 abc123_a

---
+++ atcoder.jp/abc123/abc123_a/Main.cpp
@@ -0,0 +1,23 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[5];
+    for(int i=0; i<5; i++){
+        cin>>a[i];
+    }
+    int k;
+    cin>>k;
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(abs(a[i]-a[j]) > k){
+                cout << ":(" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "Yay!" << endl;
+}
