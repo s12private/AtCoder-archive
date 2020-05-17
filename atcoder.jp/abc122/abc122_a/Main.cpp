c7ec03cbb2e4c59b75ed4b03838ee74221485fd5
Shigeki <android.s12.device@gmail.com>
2020-05-17 20:05:07 +0900

    [AC] abc122 abc122_a

---
+++ atcoder.jp/abc122/abc122_a/Main.cpp
@@ -0,0 +1,21 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string b; cin>>b;
+
+    if(b == "A"){
+        cout << "T" << endl;
+    }
+    if(b == "C"){
+        cout << "G" << endl;
+    }
+    if(b == "G"){
+        cout << "C" << endl;
+    }
+    if(b == "T"){
+        cout << "A" << endl;
+    }
+}
+
