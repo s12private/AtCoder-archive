d50df4a4f6875f743cc1366bfb53145a435387d6
Shigeki <android.s12.device@gmail.com>
2020-05-17 21:02:19 +0900

    [AC] abc168 abc168_a

---
+++ atcoder.jp/abc168/abc168_a/Main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin>>N;
+    N %= 10;
+    if(N == 2 || N == 4 || N == 5 || N == 7 || N == 9){
+        cout << "hon" << endl;
+    }else if(N == 0 || N == 1 || N == 6 || N == 8){
+cout << "pon" << endl;
+    }else{
+cout << "bon" << endl;
+    }
+}
+
