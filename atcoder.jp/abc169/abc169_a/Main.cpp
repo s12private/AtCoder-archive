b610348788413512f18db5f5da33dece6b789419
Shigeki <android.s12.device@gmail.com>
2020-06-12 01:01:25 +0900

    [AC] abc169 abc169_a

---
+++ atcoder.jp/abc169/abc169_a/Main.cpp
@@ -0,0 +1,14 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main(){
+   ll A;
+   long double B;
+
+   cin>>A>>B;
+
+   cout << (ll)(A*B) << endl;
+}
