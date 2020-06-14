97c55b0d50de2d1f8968ff61b013cea299e77ae9
Shigeki <android.s12.device@gmail.com>
2020-06-14 18:20:20 +0900

    [AC] tokiomarine2020 tokiomarine2020_a

---
+++ atcoder.jp/tokiomarine2020/tokiomarine2020_a/Main.cpp
@@ -0,0 +1,13 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main(){
+  string s; cin>>s;
+  for(int i=0; i<3; i++){
+     cout << s[i];
+  }
+  cout<<endl;
+}
