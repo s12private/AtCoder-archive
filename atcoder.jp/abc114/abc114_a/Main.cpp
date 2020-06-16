1c0ac9026cb2257f8dc011d4e760d124482994a1
Shigeki <android.s12.device@gmail.com>
2020-06-16 21:25:50 +0900

    [AC] abc114 abc114_a

---
+++ atcoder.jp/abc114/abc114_a/Main.cpp
@@ -0,0 +1,15 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    int X; cin>>X;
+    if(X == 7 || X == 5 || X == 3){
+        cout << "YES" << endl;
+    }else{
+        cout << "NO" << endl;
+    }
+}
