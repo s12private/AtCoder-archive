22f7a394fe8c44379aeb318f1712f40fdc02ac40
Shigeki <android.s12.device@gmail.com>
2020-06-16 20:50:28 +0900

    [AC] abc117 abc117_a

---
+++ atcoder.jp/abc117/abc117_a/Main.cpp
@@ -0,0 +1,13 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    int T,X;
+    cin>>T>>X;
+
+    cout << (double)T/X << endl;
+}
