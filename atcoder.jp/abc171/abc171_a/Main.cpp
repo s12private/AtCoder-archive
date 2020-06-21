92d689d73b02171f7b51c00a2583fad8d7c95b78
Shigeki <android.s12.device@gmail.com>
2020-06-21 21:11:40 +0900

    [AC] abc171 abc171_a

---
+++ atcoder.jp/abc171/abc171_a/Main.cpp
@@ -0,0 +1,23 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    char a; cin>>a;
+    char s[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char b[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+    rep(i, 26){
+        if(a == s[i]){
+            cout <<"a"<<endl;
+            break;
+        }
+        if(a == b[i]){
+            cout << "A" << endl;
+        }
+}
+
+}
