94500d5877abf749d3ed54cde0c9e421948342f5
Shigeki <android.s12.device@gmail.com>
2020-06-23 21:37:18 +0900

    [AC] abc078 arc085_a

---
+++ atcoder.jp/abc078/arc085_a/Main.cpp
@@ -0,0 +1,14 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    ll N, M;
+    cin >> N >> M;
+    ll p = pow(2, M);
+    ll t = 1900*M+100*(N-M);
+    cout << p*t << endl;
+}
