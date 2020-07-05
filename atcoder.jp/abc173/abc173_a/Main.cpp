5d77b2776e6be62d2575ff8f01dd12b1baf3c8bf
Shigeki <android.s12.device@gmail.com>
2020-07-05 21:02:12 +0900

    [AC] abc173 abc173_a

---
+++ atcoder.jp/abc173/abc173_a/Main.cpp
@@ -0,0 +1,20 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+   int N; cin>>N;
+   N %= 1000;
+   if(N != 0) N = 1000 - N;
+   cout << N << endl;
+}
