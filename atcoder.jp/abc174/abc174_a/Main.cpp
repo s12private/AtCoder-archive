f4f1ce48e23463d50e4f00bcacf29fd813f16964
Shigeki <android.s12.device@gmail.com>
2020-08-24 20:20:53 +0900

    [AC] abc174 abc174_a

---
+++ atcoder.jp/abc174/abc174_a/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+   int x;
+   cin>>x;
+   if(x >= 30){
+      cout << "Yes" << endl;
+   }else{
+      cout << "No" << endl;
+   }
+}
