e9502f3f539fa4272b9f0ce12ff5c52ff4dc9a6b
Shigeki <android.s12.device@gmail.com>
2020-11-01 21:01:02 +0900

    [AC] abc181 abc181_a

---
+++ atcoder.jp/abc181/abc181_a/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    ll N; cin>>N;
+    if(N%2 == 0){
+        cout << "White" << endl;
+    }else{
+        cout << "Black" << endl;
+    }
+
+}
