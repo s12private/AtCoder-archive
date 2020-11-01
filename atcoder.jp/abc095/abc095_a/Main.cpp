a0d1812c9b24762c392c32333511b92d063d1598
Shigeki <android.s12.device@gmail.com>
2020-11-01 20:52:50 +0900

    [AC] abc095 abc095_a

---
+++ atcoder.jp/abc095/abc095_a/Main.cpp
@@ -0,0 +1,15 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+int main(){
+    string s; cin>>s;
+    ll count = 0;
+    for(char c : s){
+        if(c == 'o') count++;
+    }
+    cout << 700+100*count << endl;
+}
