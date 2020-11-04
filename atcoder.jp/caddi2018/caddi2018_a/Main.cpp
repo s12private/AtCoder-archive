b7d9de306aeaa16d3cb5e099e8b5b2d3c43bbc8c
Shigeki <android.s12.device@gmail.com>
2020-11-05 00:12:31 +0900

    [AC] caddi2018 caddi2018_a

---
+++ atcoder.jp/caddi2018/caddi2018_a/Main.cpp
@@ -0,0 +1,34 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define FOR(i,a,n) for(ll i=a;i<(ll)(n);i++)
+
+vector<long long> enum_divisors(long long N) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+            res.push_back(i);
+            // 重複しないならば i の相方である N/i も push
+            if (N/i != i) res.push_back(N/i);
+        }
+    }
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    ll N, P; cin>>N>>P;
+
+    auto div = enum_divisors(P);
+    sort(div.rbegin(), div.rend());
+
+    for(ll a : div){
+        if(P%(ll)pow(a, N) == 0){
+            cout << a << endl;
+            return 0;
+        }
+    }
+}
