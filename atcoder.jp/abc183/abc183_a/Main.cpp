abb864a12881260967641327ae8481ebc00e15c6
Shigeki <android.s12.device@gmail.com>
2020-11-15 21:00:43 +0900

    [AC] abc183 abc183_a

---
+++ atcoder.jp/abc183/abc183_a/Main.cpp
@@ -0,0 +1,11 @@
+#define _GLIBCXX_DEBUG
+#define _GLIBCXX_DEBUG_PEDANTIC
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+    ll x; cin>>x;
+    cout << (x<0 ? 0 : x) << endl;
+}
