3f42721522939533da54ebd1c7f15d7b92028dc4
Shigeki <android.s12.device@gmail.com>
2020-11-21 02:04:21 +0900

    [AC] arc030 arc030_1

---
+++ atcoder.jp/arc030/arc030_1/Main.cpp
@@ -0,0 +1,11 @@
+#define _GLIBCXX_DEBUG
+#define _GLIBCXX_DEBUG_PEDANTIC
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+    ll n,k; cin>>n>>k;
+    cout << (n/2 >= k ? "YES" : "NO") << endl;
+}
