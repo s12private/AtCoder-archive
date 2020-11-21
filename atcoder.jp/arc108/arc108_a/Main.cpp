279e40cf01b718e1e74fb5ec6e766723caa99f94
Shigeki <android.s12.device@gmail.com>
2020-11-22 01:01:55 +0900

    [AC] arc108 arc108_a

---
+++ atcoder.jp/arc108/arc108_a/Main.cpp
@@ -0,0 +1,34 @@
+#define _GLIBCXX_DEBUG
+#define _GLIBCXX_DEBUG_PEDANTIC
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+vector<long long> enum_divisors(long long N) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+            res.push_back(i);
+            // 重複しないならば i の相方である N/i も push
+            if (N/i != i) res.push_back(N/i);
+        }
+    }
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
+}
+
+int main() {
+    ll S, P; cin>>S>>P;
+
+    for(ll N : enum_divisors(P)){
+        ll M = P/N;
+        if(N+M == S){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+}
