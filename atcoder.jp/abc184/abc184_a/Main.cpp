d47ed5830586cd6aeaa7eb2acc40a74a6744efdb
Shigeki <android.s12.device@gmail.com>
2020-11-22 21:00:33 +0900

    [AC] abc184 abc184_a

---
+++ atcoder.jp/abc184/abc184_a/Main.cpp
@@ -0,0 +1,10 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+    ll a,b,c,d; cin>>a>>b>>c>>d;
+    cout << a*d-b*c << endl;
+}
