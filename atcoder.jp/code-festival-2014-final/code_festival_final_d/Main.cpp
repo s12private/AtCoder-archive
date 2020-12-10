2c74d8d46899f19fd6c861c2e3f17851773f6f50
Shigeki <android.s12.device@gmail.com>
2020-12-10 17:18:14 +0900

    [AC] code-festival-2014-final code_festival_final_d

---
+++ atcoder.jp/code-festival-2014-final/code_festival_final_d/Main.cpp
@@ -0,0 +1,12 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+
+int main() {
+    ll A; cin>>A;
+
+    cout << A+1 << " " << 2 << endl;
+}
