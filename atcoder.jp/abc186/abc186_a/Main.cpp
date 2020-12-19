c33383547473c33a13f3b2761694c578eb319023
Shigeki <android.s12.device@gmail.com>
2020-12-19 21:00:42 +0900

    [AC] abc186 abc186_a

---
+++ atcoder.jp/abc186/abc186_a/Main.cpp
@@ -0,0 +1,12 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+
+int main() {
+    ll N, W; cin>>N>>W;
+    cout << N/W << endl;
+}
