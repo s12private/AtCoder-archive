197d7b8971411ae3df4c9abeefc0449360d256ce
Shigeki <android.s12.device@gmail.com>
2021-02-01 05:49:48 +0900

    [AC] abc112 abc112_d

---
+++ atcoder.jp/abc112/abc112_d/Main.cpp
@@ -0,0 +1,21 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N, M; cin>>N>>M;
+    
+    for(ll i=M/N; i>0; i--){
+        ll a = i*(N-1);
+        if((M-a)%i == 0){
+            cout << i << endl;
+            return 0;
+        }
+    }
+}
