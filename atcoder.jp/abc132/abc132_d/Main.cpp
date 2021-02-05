14a8f00868ab3afe6437462ce55bca36da868c63
Shigeki <android.s12.device@gmail.com>
2021-02-06 03:04:23 +0900

    [AC] abc132 abc132_d

---
+++ atcoder.jp/abc132/abc132_d/Main.cpp
@@ -0,0 +1,50 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+using namespace std;
+using namespace boost::multiprecision;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+long long fac[MAX], finv[MAX], inv[MAX];
+
+// テーブルを作る前処理
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+// 二項係数計算
+long long nCk(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+int main() {
+    ll N, K; cin>>N>>K;
+    COMinit();
+    rep(i, K){
+        if(N == K && i == 0){
+            cout << 1 << endl;
+            continue;
+        }
+        ll res = nCk(N-K-1, i-1) + 2*nCk(N-K-1, i) + nCk(N-K-1, i+1);
+        res %= MOD;
+        res *= nCk(K-1, i);
+        res %= MOD;
+        cout << res << endl;
+    }
+}
