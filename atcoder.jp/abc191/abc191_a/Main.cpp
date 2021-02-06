fcbd7a011c857ceb1123db542d30d1f9f3c2cfc9
Shigeki <android.s12.device@gmail.com>
2021-02-06 21:01:34 +0900

    [AC] abc191 abc191_a

---
+++ atcoder.jp/abc191/abc191_a/Main.cpp
@@ -0,0 +1,20 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+using namespace std;
+using namespace boost::multiprecision;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll V,T,S,D; cin>>V>>T>>S>>D;
+    if(D >= V*T && D <= V*S){
+        cout << "No" << endl;
+    }else{
+        cout << "Yes" << endl;
+    }
+}
