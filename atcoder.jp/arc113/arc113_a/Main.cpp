50479b47080443facd41f2f5471b245fe5b8646b
Shigeki <android.s12.device@gmail.com>
2021-02-22 01:21:47 +0900

    [AC] arc113 arc113_a

---
+++ atcoder.jp/arc113/arc113_a/Main.cpp
@@ -0,0 +1,22 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <atcoder/all>
+using namespace std;
+using namespace boost::multiprecision;
+using namespace atcoder;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll K; cin>>K;
+    ll res = 0;
+    for(ll a=1; a<=K; a++){
+        for(ll b=1; a*b<=K; b++){
+            res += K/(a*b);
+        }
+    }
+    cout << res << endl;
+}
