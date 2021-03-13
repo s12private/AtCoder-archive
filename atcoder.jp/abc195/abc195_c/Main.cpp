8453d9528ff9777033772191729fddd9cf9020d3
Shigeki <android.s12.device@gmail.com>
2021-03-13 21:32:57 +0900

    [AC] abc195 abc195_c

---
+++ atcoder.jp/abc195/abc195_c/Main.cpp
@@ -0,0 +1,19 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
+using namespace std;
+using namespace boost::multiprecision;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N; cin>>N;
+
+    ll res = 0;
+    for(ll i=3; pow(10, i)<=N; i+=3){
+        res += N-pow(10, i)+1;
+    }
+    cout << res << endl;
+}
