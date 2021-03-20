3b01ed956fc7106b64acd1d982b96e894abb5c9f
Shigeki <android.s12.device@gmail.com>
2021-03-20 21:01:40 +0900

    [AC] abc196 abc196_a

---
+++ atcoder.jp/abc196/abc196_a/Main.cpp
@@ -0,0 +1,12 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll a,b,c,d; cin>>a>>b>>c>>d;
+    cout << max({a-c, a-d, b-c, b-d}) << endl;
+}
