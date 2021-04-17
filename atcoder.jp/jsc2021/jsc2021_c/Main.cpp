41803be6ba1a574cf64d1874a3a118e2a75ab3cc
Shigeki <android.s12.device@gmail.com>
2021-04-17 17:16:59 +0900

    [AC] jsc2021 jsc2021_c

---
+++ atcoder.jp/jsc2021/jsc2021_c/Main.cpp
@@ -0,0 +1,38 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+vector<long long> enum_divisors(long long N) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+            res.push_back(i);
+            // 重複しないならば i の相方である N/i も push
+            if (N/i != i) res.push_back(N/i);
+        }
+    }
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
+}
+
+
+int main() {
+    ll A, B; cin>>A>>B;
+
+    vector<ll> a(B+1);
+    ll mx = 1;
+    for(ll i=A; i<=B; i++){
+        auto p = enum_divisors(i);
+        for(ll l : p){
+            if(a[l]>0) chmax(mx, l);
+            a[l]++;
+
+        }
+    }
+    cout << mx << endl;
+}
