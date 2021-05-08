ebe9c3dce9ac524e60fd5cf33f34db45a187d2f6
Shigeki <android.s12.device@gmail.com>
2021-05-08 21:03:16 +0900

    [AC] abc200 abc200_b

---
+++ atcoder.jp/abc200/abc200_b/Main.cpp
@@ -0,0 +1,18 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N, K; cin>>N>>K;
+    rep(i, K){
+        if(N%200 == 0) N /=200;
+        else{
+            N = N*1000+200;
+        }
+    }
+    cout << N << endl;
+}
