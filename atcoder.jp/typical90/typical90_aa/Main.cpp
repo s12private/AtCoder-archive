3797e91ac27d58ef07812076707a3e997e8353ed
Shigeki <android.s12.device@gmail.com>
2021-05-31 09:06:52 +0900

    [AC] typical90 typical90_aa

---
+++ atcoder.jp/typical90/typical90_aa/Main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N; cin>>N;
+    map<string, bool> m;
+    rep(i, N){
+        string s; cin>>s;
+        if(!m[s]) cout << i+1 << endl;
+        m[s] = true;
+    }
+}
