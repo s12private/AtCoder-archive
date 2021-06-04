d42e1046ed3ae8ce546e6922c84c75f72a57a1a9
Shigeki <android.s12.device@gmail.com>
2021-06-04 21:26:14 +0900

    [AC] abc185 abc185_a

---
+++ atcoder.jp/abc185/abc185_a/Main.cpp
@@ -0,0 +1,14 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    vector<ll> A(4);
+    cin>>A[0]>>A[1]>>A[2]>>A[3];
+    sort(A.begin(), A.end());
+    cout << A[0] << endl;
+}
