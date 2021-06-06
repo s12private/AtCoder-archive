e4820b1288b5f19b2281a57ac2a4c28da0792f70
Shigeki <android.s12.device@gmail.com>
2021-06-06 21:03:18 +0900

    [AC] abc204 abc204_b

---
+++ atcoder.jp/abc204/abc204_b/Main.cpp
@@ -0,0 +1,18 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N; cin>>N;
+
+    ll res = 0;
+    rep(i, N){
+        ll A; cin>>A;
+        if(A > 10) res += A-10;
+    }
+    cout << res << endl;
+}
