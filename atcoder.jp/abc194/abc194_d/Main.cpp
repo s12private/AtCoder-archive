26f114d7932dcf854661ab79c3b3df98dc0940ac
Shigeki <android.s12.device@gmail.com>
2021-06-09 19:48:49 +0900

    [AC] abc194 abc194_d

---
+++ atcoder.jp/abc194/abc194_d/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N; cin>>N;
+    double res = 0;
+    for(ll i=1; i<N; i++){
+        res += (double)N/(N-i);
+    }
+    cout << fixed << setprecision(10) << res << endl;
+}
