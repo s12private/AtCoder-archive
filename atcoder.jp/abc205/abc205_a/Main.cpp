a6c28c62e4a3143bc193dada021bf5de47cebc0c
Shigeki <android.s12.device@gmail.com>
2021-06-13 21:01:24 +0900

    [AC] abc205 abc205_a

---
+++ atcoder.jp/abc205/abc205_a/Main.cpp
@@ -0,0 +1,14 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll A,B; cin>>A>>B;
+    double res = A/100.0*B;
+
+    cout << fixed << setprecision(10) << res << endl;
+}
