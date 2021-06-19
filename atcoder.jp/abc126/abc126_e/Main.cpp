63171b1509a4f90a6e0aa4dee42f0acc3433c0e8
Shigeki <android.s12.device@gmail.com>
2021-06-20 01:02:23 +0900

    [AC] abc126 abc126_e

---
+++ atcoder.jp/abc126/abc126_e/Main.cpp
@@ -0,0 +1,86 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+class UnionFind{
+public:
+    vector<ll> parent; //parent[i]はiの親
+    vector<ll> siz; //素集合のサイズを表す配列(1で初期化)
+    map<ll,vector<ll>> group; //集合ごとに管理する(key:集合の代表元、value:集合の要素の配列)
+    ll n; //要素数
+
+    //コンストラクタ
+    UnionFind(ll n_):n(n_),parent(n_),siz(n_,1){
+        //全ての要素の根が自身であるとして初期化
+        for(ll i=0;i<n;i++){parent[i]=i;}
+    }
+
+    //データxの属する木の根を取得(経路圧縮も行う)
+    ll root(ll x){
+        if(parent[x]==x) return x;
+        return parent[x]=root(parent[x]);//代入式の値は代入した変数の値なので、経路圧縮できる
+    }
+
+    //xとyの木を併合
+    void unite(ll x,ll y){
+        ll rx=root(x);//xの根
+        ll ry=root(y);//yの根
+        if(rx==ry) return;//同じ木にある時
+        //小さい集合を大きい集合へと併合(ry→rxへ併合)
+        if(siz[rx]<siz[ry]) swap(rx,ry);
+        siz[rx]+=siz[ry];
+        parent[ry]=rx;//xとyが同じ木にない時はyの根ryをxの根rxにつける
+    }
+
+    //xとyが属する木が同じかを判定
+    bool same(ll x,ll y){
+        ll rx=root(x);
+        ll ry=root(y);
+        return rx==ry;
+    }
+
+    //xの素集合のサイズを取得
+    ll size(ll x){
+        return siz[root(x)];
+    }
+
+    //素集合をそれぞれグループ化
+    void grouping(){
+        //経路圧縮を先に行う
+        rep(i,n)root(i);
+        //mapで管理する(デフォルト構築を利用)
+        rep(i,n)group[parent[i]].push_back(i);
+    }
+
+    //素集合系を削除して初期化
+    void clear(){
+        rep(i,n){parent[i]=i;}
+        siz=vector<ll>(n,1);
+        group.clear();
+    }
+
+    //連結成分の個数
+    ll size(){
+        ll res=0;
+        rep(i,n) if(root(i) == i) res++;
+        return res;
+    }
+};
+
+int main() {
+    ll N,M; cin>>N>>M;
+    UnionFind uf(N);
+    rep(i, M){
+        ll X,Y,Z; cin>>X>>Y>>Z;
+        uf.unite(X-1, Y-1);
+    }
+    ll res = 0;
+    rep(i, N){
+        if(uf.root(i) == i) res++;
+    }
+    cout << uf.size() << endl;
+}
