9e83eecf9b9b91de30827b5b51c0dc8604c38dc6
Shigeki <android.s12.device@gmail.com>
2021-06-26 21:01:08 +0900

    [AC] abc207 abc207_a

---
+++ atcoder.jp/abc207/abc207_a/Main.cpp
@@ -0,0 +1,14 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    vector<ll> a(3);
+    cin>>a[0]>>a[1]>>a[2];
+    sort(a.rbegin(), a.rend());
+    cout << (a[0]+a[1]) << endl;
+}
