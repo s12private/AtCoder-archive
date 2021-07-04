162c3c07188df8a2e798db2c698fecb8039dc63b
Shigeki <android.s12.device@gmail.com>
2021-07-04 21:06:24 +0900

    [AC] abc208 abc208_a

---
+++ atcoder.jp/abc208/abc208_a/Main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll A,B; cin>>A>>B;
+
+    if(6*A >= B && !(A > B)){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
