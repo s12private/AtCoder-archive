06dead748c661f9ad42ac7af3e44919665d2b4e4
Shigeki <android.s12.device@gmail.com>
2021-07-17 21:02:02 +0900

    [AC] abc210 abc210_a

---
+++ atcoder.jp/abc210/abc210_a/Main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N,A,X,Y; cin>>N>>A>>X>>Y;
+    ll sum = 0;
+    if(N <= A){
+        cout << N*X << endl;
+    }else{
+        cout << A*X + (N-A)*Y << endl;
+    }
+}
