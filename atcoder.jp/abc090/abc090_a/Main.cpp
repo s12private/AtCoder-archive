1a082ece1d30d00de89378d0e94acc169513f0c8
Shigeki <android.s12.device@gmail.com>
2021-07-24 20:44:13 +0900

    [AC] abc090 abc090_a

---
+++ atcoder.jp/abc090/abc090_a/Main.cpp
@@ -0,0 +1,15 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    rep(i, 3){
+        string s; cin>>s;
+        cout << s[i];
+    }
+    cout << endl;
+}
