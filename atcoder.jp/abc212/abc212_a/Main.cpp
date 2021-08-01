e99d5332ddcbfe5a715c8d7635395307df637906
Shigeki <android.s12.device@gmail.com>
2021-08-01 20:49:55 +0900

    [AC] abc212 abc212_a

---
+++ atcoder.jp/abc212/abc212_a/Main.cpp
@@ -0,0 +1,18 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll A, B; cin>>A>>B;
+    if(0 < A && B == 0){
+        cout << "Gold" << endl;
+    }else if(A == 0 && 0 < B){
+        cout << "Silver" << endl;
+    }else{
+        cout << "Alloy" << endl;
+    }
+}
