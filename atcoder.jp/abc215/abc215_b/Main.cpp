7b2fb230abd5f9ab273f57872461d2cafff77329
Shigeki <android.s12.device@gmail.com>
2021-08-21 21:03:20 +0900

    [AC] abc215 abc215_b

---
+++ atcoder.jp/abc215/abc215_b/Main.cpp
@@ -0,0 +1,20 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll N; cin>>N;
+
+    ll a = 1;
+    for(ll i=0; i<100; i++){
+        if(a > N){
+            cout << i-1 << endl;
+            return 0;
+        }
+        a *= 2;
+    }
+}
