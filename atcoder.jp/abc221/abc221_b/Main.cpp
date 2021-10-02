957b7cd29b4053b86427a7f1a9ba0a38e527c1ad
Shigeki <android.s12.device@gmail.com>
2021-10-02 21:06:33 +0900

    [AC] abc221 abc221_b

---
+++ atcoder.jp/abc221/abc221_b/Main.cpp
@@ -0,0 +1,27 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    string S, T; cin>>S>>T;
+
+    if(S == T){
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    rep(i, S.size()-1) {
+        string s = S;
+        s[i] = S[i+1];
+        s[i+1] = S[i];
+        if(s == T){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
