5315857ef76dffc175a3f6275104115407f398ae
Shigeki <android.s12.device@gmail.com>
2021-10-12 22:45:12 +0900

    [AC] abc222 abc222_a

---
+++ atcoder.jp/abc222/abc222_a/Main.cpp
@@ -0,0 +1,15 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    string N; cin>>N;
+    rep(i, 4-N.size()) {
+        cout << "0";
+    }
+    cout <<N << endl;
+}
