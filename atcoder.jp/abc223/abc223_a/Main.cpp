2a82ba285ba2096ab0522c3f6d60c638d9461524
Shigeki <android.s12.device@gmail.com>
2021-10-20 19:26:39 +0900

    [AC] abc223 abc223_a

---
+++ atcoder.jp/abc223/abc223_a/Main.cpp
@@ -0,0 +1,16 @@
+//#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+
+int main() {
+    ll X; cin>>X;
+    if(X%100 == 0 && X != 0) {
+        cout << "Yes" << endl;
+    }else {
+        cout << "No" << endl;
+    }
+}
